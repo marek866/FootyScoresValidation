cb4e1f97d0a00a1a1db511df746aafc52b4f3001
Marek Koseski <marekkoseski@hype4mobile.com>
2026-05-02 13:49:14 +0200

    Update dependencies, add Prettier configuration and Introduce testing setup with Vitest and create initial test for App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Footy Scraper Validator</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-})
+  test: {
+    environment: "happy-dom",
+    setupFiles: "./src/test/setup.ts",
+  },
+});
